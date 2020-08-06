4597cc817a74a7cd6001f3690e9c429a0a3f86fa
Nitin Saroha <nitinsaroha@outlook.com>
2020-08-05 22:56:16 -0700

    Removed Admin Page

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -7,7 +7,6 @@ import SignInPage from '../SignIn';
 import PasswordForgetPage from '../PasswordForget';
 import HomePage from '../Home';
 import AccountPage from '../Account';
-import AdminPage from '../Admin';
 
 import * as ROUTES from '../../constants/routes';
 import { withAuthentication } from '../Session';
@@ -24,7 +23,6 @@ const App = () => (
       <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
       <Route path={ROUTES.HOME} component={HomePage} />
       <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-      <Route path={ROUTES.ADMIN} component={AdminPage} />
     </div>
   </Router>
 );
